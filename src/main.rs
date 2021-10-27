19f2c5f920f8ec0750f12a9c1261b3814d860b12
techno-tanoC <develop.tanoc@gmail.com>
2021-10-27 20:43:56 +0900

    Update tests

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,113 @@
+#[derive(Debug, PartialEq, Clone)]
+struct Foo {
+    boolean: bool,
+    number: i64,
+    string: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use futures::future::FutureExt;
+    use pretty_assertions::{assert_eq, assert_ne};
     use std::panic;
 
-    #[tokio::test]
-    async fn test_success() {
-        run_test(async {
-            tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-            assert!(true);
-        }).await;
+    #[test]
+    fn test_sample() {
+        assert_eq!(
+            Foo { boolean: true, number: 0, string: "Hello".to_string() },
+            Foo { boolean: false, number: 1, string: "Hello World".to_string() }
+        );
     }
 
-    #[tokio::test]
-    async fn test_failure() {
-        run_test(async {
-            tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-            assert!(false);
-        }).await;
+    #[test]
+    fn test_success() {
+        run_test(|| {
+            assert_ne!(
+                Foo { boolean: true, number: 0, string: "Hello".to_string() },
+                Foo { boolean: false, number: 1, string: "Hello World".to_string() }
+            );
+        });
     }
 
-    async fn setup() {
+    #[test]
+    fn test_failure() {
+        run_test(|| {
+            assert_eq!(
+                Foo { boolean: true, number: 0, string: "Hello".to_string() },
+                Foo { boolean: false, number: 1, string: "Hello World".to_string() }
+            );
+        });
+    }
+
+    fn setup() {
         println!("before setup");
-        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+        std::thread::sleep(std::time::Duration::from_secs(1));
         println!("after setup");
     }
 
-    async fn teardown() {
-        println!("before teardown");
-        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-        println!("after teardown");
+    fn teardown() {
+        println!("before teeardown");
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        println!("after teeardown");
     }
 
-    async fn run_test<T>(test: T)
+    fn run_test<F>(f: F)
     where
-        T: std::future::Future + Send + 'static,
-        T::Output: Send + 'static,
+        F: FnOnce(),
     {
-        setup().await;
+        setup();
+        let result = panic::catch_unwind(panic::AssertUnwindSafe(f));
+        teardown();
 
+        if let Err(err) = result {
+            panic::resume_unwind(err);
+        }
+    }
+
+    #[tokio::test]
+    async fn test_success_async() {
+        run_test_async(async {
+            assert_ne!(
+                Foo { boolean: true, number: 0, string: "Hello".to_string() },
+                Foo { boolean: false, number: 1, string: "Hello World".to_string() },
+            );
+        }).await;
+    }
+
+    #[tokio::test]
+    async fn test_failure_async() {
+        run_test_async(async {
+            assert_eq!(
+                Foo { boolean: true, number: 0, string: "Hello".to_string() },
+                Foo { boolean: false, number: 1, string: "Hello World".to_string() },
+            );
+        }).await;
+    }
+
+    async fn setup_async() {
+        println!("before setup_async");
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+        println!("after setup_async");
+    }
+
+    async fn teardown_async() {
+        println!("before teardown_async");
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+        println!("after teardown_async");
+    }
+
+    async fn run_test_async<F>(test: F)
+    where
+        F: std::future::Future,
+    {
+        setup_async().await;
         let result = panic::AssertUnwindSafe(test).catch_unwind().await;
-
-        teardown().await;
+        teardown_async().await;
 
         if let Err(err) = result {
             panic::resume_unwind(err);
